0731c26025ebec03b061a10db4bb0cab438d3d84
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-04-30 09:53:55 +0300

    Updates

---
+++ precache-manifest.ef094c27572b81e704fef45cb4565d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b81ff5bf142cdbdcb49ea6fc5f780ef",
+    "url": "/middle.react.praktikum.yandex/index.html"
+  },
+  {
+    "revision": "ff3f101ec94593ed1cf4",
+    "url": "/middle.react.praktikum.yandex/static/css/main.7a5efba0.chunk.css"
+  },
+  {
+    "revision": "8743d6404dfe0a19f431",
+    "url": "/middle.react.praktikum.yandex/static/js/2.6dddcb5f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/middle.react.praktikum.yandex/static/js/2.6dddcb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3f101ec94593ed1cf4",
+    "url": "/middle.react.praktikum.yandex/static/js/main.b65190ae.chunk.js"
+  },
+  {
+    "revision": "c7ce702d4e73cec005f5",
+    "url": "/middle.react.praktikum.yandex/static/js/runtime-main.5876ed40.js"
+  }
+]);
